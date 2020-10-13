82a00c3ffbc6bd09f77bc19749ee81775e3fd3fb
YYB <1755916756@qq.com>
2020-10-13 14:56:41 +0800

    element-updata

--- element/src/router/index.js
+++ element/src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Element from "@/components/element";
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/e",
+      name: "e",
+      component: Element
     }
   ]
-})
+});
